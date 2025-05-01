a76894f75a867e798848c15ffcb64b592e03120f
AgustinBustos <agustinbustosbarton@gmail.com>
2025-05-01 12:06:13 -0300

    litle

---
+++ scrp.py
@@ -0,0 +1,103 @@
+from tkinter import *
+import tkinter as tk
+import pyautogui
+from pynput import keyboard
+
+import webbrowser
+import time
+
+global_thing=[]
+def on_press(key):
+    try:
+        global_thing.append(pyautogui.position())
+    except AttributeError:
+        print('special key {0} pressed'.format(key))
+
+
+def on_release(key):
+    if key == keyboard.Key.space:
+        return False
+with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
+
+    listener.join()
+
+
+currentMouseX, currentMouseY=global_thing[-1]
+
+def exit_app(event=None):
+    root.destroy()
+
+root = tk.Tk()
+# root.attributes('-fullscreen', True)
+root.attributes('-topmost', True)
+root.attributes('-alpha', 1)
+root.configure(bg='black')
+root.overrideredirect(True)
+
+root.bind("<Escape>", exit_app)
+from difflib import get_close_matches
+
+items = ['apple', 'banana', 'grapefruit', 'orange', 'pineapple', 'blueberry', 'apricot']
+
+
+
+
+# Create a frame with fixed size
+frame = tk.Frame(root, width=currentMouseX, height=currentMouseY,bg='black')
+frame.pack()
+frame.pack_propagate(False)  # Prevent shrinking to fit contents
+
+
+canvas = tk.Canvas(frame, width=currentMouseX, height=currentMouseY, bg='black', highlightthickness=0)
+
+
+def update_listbox(*args):
+    search_term = entry_var.get()
+    matches = [i for i in items if search_term in i] #get_close_matches(search_term, items, n=10, cutoff=0.3)
+    
+    listbox.delete(0, tk.END)
+    for item in matches if search_term else items:
+        listbox.insert(tk.END, item)
+
+entry_var = tk.StringVar()
+entry_var.trace("w", update_listbox)  # Trigger update on change
+
+entry = tk.Entry(frame, textvariable=entry_var)
+entry.pack()
+
+listbox = tk.Listbox(frame,height=8)
+# items = ["Python"+str(i) for i in range(100)]
+    
+
+# Agregar elementos almacenados en una lista o tupla.
+listbox.insert(0, *items)
+def on_select(event):
+    selected_indices = listbox.curselection()
+    if selected_indices:
+        selected_item = listbox.get(selected_indices[0])
+        print(f"Selected item: {selected_item}")
+        if selected_item=='apple':
+            message_label.config(text="Correct", fg="green") 
+            message_label.after(2000, lambda: message_label.config(text=""))  # Hide after 2 seconds
+        else:
+            message_label.config(text="Incorrect", fg="red")
+            message_label.after(2000, lambda: message_label.config(text=""))  # Hide after 2 seconds
+
+listbox.bind('<<ListboxSelect>>', on_select)
+
+
+
+message_label = tk.Label(frame, text="", font=("Arial", 16))
+# def show_correct():
+
+
+
+listbox.pack()
+message_label.pack()
+canvas.pack()
+webbrowser.open("https://www.google.com")
+root.mainloop()
+
+
+
+
